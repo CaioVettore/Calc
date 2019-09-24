5112f6a17dc3f36e4b1e2b6eec0c318540b1d95e
Vettore0 <54145455+Vettore0@users.noreply.github.com>
2019-09-24 11:44:11 -0300

    att

--- js/index.js
+++ js/index.js
@@ -11,6 +11,45 @@ function fazerRequisicao(){
     var retorno = JSON.parse(xhttp.responseText);
     console.log(xhttp.responseText);
 
-    var primeiroNome = retorno.results[1].name;
-    document.getElementById("nome1").textContent = primeiroNome;
+    //var primeiroNome = retorno.results[1].name;
+    //document.getElementById("nome1").textContent = primeiroNome;
+    var html = "";
+    for(var i = 0; i < retorno.results.length; i++){
+        var nome = retorno.results[i].name;
+        var urlusuario = retorno.results[i].url;
+        // html = html + '<a href="#" onclick="fazerRequisicao"('+url+');"<br>";
+        html = html + "<a href='#' onclick=Dados('"+urlusuario+"')>"+ nome +"</a><br>"
+            
+    }
+    document.getElementById("usuarios").innerHTML = html;
+    
 }
+
+function Dados(url){
+
+
+    var urlusuario = url;
+   
+    
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", urlusuario, false);
+
+    xhttp.send();//a execução do script pára aqui até a requi
+
+    var retorno = JSON.parse(xhttp.responseText);
+    console.log(xhttp.responseText);
+    var html = "";
+
+    var nome = retorno.name;
+    var altura = retorno.height;
+    var peso = retorno.mass;
+    var cabelo = retorno.hair_color;
+    var cor_da_pele = retorno.skin_color;
+
+        
+    html = html + nome +" "+ altura +" " + peso +" " + cabelo +" " + cor_da_pele;
+    document.getElementById("detalhesusu").innerHTML = html;
+      
+}
+
