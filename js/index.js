76a37aafb6200ef24aa768cc7c485fa3d4d179da
Vettore0 <54145455+Vettore0@users.noreply.github.com>
2019-09-25 11:40:26 -0300

    att

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-function fazerRequisicao(){
+function fazerRequisicao() {
 
-    var url='https://swapi.co/api/people/';
-    
+    var url = 'https://swapi.co/api/people/';
+
     var xhttp = new XMLHttpRequest();
 
     xhttp.open("GET", url, false);
@@ -9,28 +9,28 @@ function fazerRequisicao(){
     xhttp.send();//a execução do script pára aqui até a requi
 
     var retorno = JSON.parse(xhttp.responseText);
-    console.log(xhttp.responseText);
+ ;
 
     //var primeiroNome = retorno.results[1].name;
     //document.getElementById("nome1").textContent = primeiroNome;
     var html = "";
-    for(var i = 0; i < retorno.results.length; i++){
+    for (var i = 0; i < retorno.results.length; i++) {
         var nome = retorno.results[i].name;
-        var urlusuario = retorno.results[i].url;
+        urlusuario = retorno.results[i].url;
         // html = html + '<a href="#" onclick="fazerRequisicao"('+url+');"<br>";
-        html = html + "<a href='#' onclick=Dados('"+urlusuario+"')>"+ nome +"</a><br>"
-            
+        html = html + "<a href='#' onclick=Dados('" + urlusuario + "')>" + nome + "</a><br>"
+
     }
     document.getElementById("usuarios").innerHTML = html;
-    
+
 }
 
-function Dados(url){
+function Dados(url) {
 
 
     var urlusuario = url;
-   
-    
+
+
     var xhttp = new XMLHttpRequest();
 
     xhttp.open("GET", urlusuario, false);
@@ -38,18 +38,55 @@ function Dados(url){
     xhttp.send();//a execução do script pára aqui até a requi
 
     var retorno = JSON.parse(xhttp.responseText);
-    console.log(xhttp.responseText);
+    
     var html = "";
 
-    var nome = retorno.name;
-    var altura = retorno.height;
-    var peso = retorno.mass;
-    var cabelo = retorno.hair_color;
-    var cor_da_pele = retorno.skin_color;
+    var filmes = [];
 
+ 
+
+    
+
+    
+
+    html = html + "<a ("+urlusuario+")>" 
+    +"<br>"+ "Name: "+ retorno.name
+    +"<br>"+ "Height: "+ retorno.height
+    +"<br>"+ "Mass: "+ retorno.mass
+    +"<br>"+ "Hair color: "+ retorno.hair
+    +"<br>"+ "Skin color: "+ retorno.skin_color +"</a><br>"
         
-    html = html + nome +" "+ altura +" " + peso +" " + cabelo +" " + cor_da_pele;
     document.getElementById("detalhesusu").innerHTML = html;
-      
-}
 
+}
+function Requisicao() {
+    var urlFilms = 'https://swapi.co/api/films/';
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", urlFilms, false);
+
+    xhttp.send();
+
+    var retorno = JSON.parse(xhttp.responseText);
+   
+
+    var html = "";
+    html = html + "<a ("+urlusuario+")>" 
+    +"<br>"+ "Title: "+ retorno.title
+    +"<br>"+ "Number : "+ retorno.episode_id
+    +"<br>"+ "Mass: "+ retorno.mass
+    +"<br>"+ "Hair color: "+ retorno.hair
+    +"<br>"+ "Skin color: "+ retorno.skin_color +"</a><br>"
+
+    for (var i = 0; i < retorno.results.length; i++) {
+        var count = retorno.results[i].title;
+        var urlFilms = retorno.results[i].url;
+        // html = html + '<a href="#" onclick="fazerRequisicao"('+url+');"<br>";
+        html = html + "<a href='#' onclick=Dados('" + urlFilms + "')>" + count + "</a><br>"
+
+    }
+    document.getElementById("films").innerHTML = html;
+
+
+}
